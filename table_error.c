acfd3a59d30dbd424360595ff42d0ee08c5789c2
Karina Aksenovskaya <karinaksenovskaya@meow.com>
2022-03-12 20:37:57 +0300

    Lab2

--- table_error.c
+++ table_error.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char *argv[]){
     for(int i=0; i<134; i++){
-        printf("Error %d: %s\n", i, strerror(i));
+        printf("Error %d: %s\n", i, strerror(i)); // не понимаю как вывести название ошибок
     }
 }
